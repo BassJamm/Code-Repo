f8a2cd6d988a63164df4fc2aa124929bc8a138bf
WillH <75690476+BassJamm@users.noreply.github.com>
2023-01-10 21:10:39 +0000

    Update Basic_Windows_info.ps1

--- PowerShell/Windows OS/Basic_Windows_info.ps1
+++ PowerShell/Windows OS/Basic_Windows_info.ps1
@@ -1,22 +1,26 @@
+<#
+Collect basic information useful in troubleshooting of a Windows device.
+#>
+
 # Create folder for all files
 New-Item "C:\SystemDiagnosticCollection" -itemType Directory
 Write-host 'Folder created at C:\SystemDiagnosticCollection' #not finished yet, it'll print out only.
 
 Write-host 'Basic AAD Info'
-dsregcmd /status | select-string -Pattern 'Device Name' 
-dsregcmd /status | select-string -Pattern 'AzureADJoined'
-dsregcmd /status | select-string -Pattern 'DeviceId'
-dsregcmd /status | select-string -Pattern 'TenantName'
+dsregcmd /status | select-string -Pattern 'Device Name'  | Out-File -FilePath C:\SystemDiagnosticCollection\Basic-AAD-Info.txt
+dsregcmd /status | select-string -Pattern 'AzureADJoined' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\Basic-AAD-Info.txt
+dsregcmd /status | select-string -Pattern 'DeviceId' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\Basic-AAD-Info.txt
+dsregcmd /status | select-string -Pattern 'TenantName' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\Basic-AAD-Info.txt
 
 write-host 'Single Sign on Info'
-dsregcmd /status | select-string -Pattern 'AzureAdPrt'
-dsregcmd /status | select-string -Pattern 'AzureAdPrtUpdateTime'
-dsregcmd /status | select-string -Pattern 'RefreshPrtDiagnostics'
+dsregcmd /status | select-string -Pattern 'AzureAdPrt' | Out-File -FilePath C:\SystemDiagnosticCollection\Single-Sign-on-Info.txt
+dsregcmd /status | select-string -Pattern 'AzureAdPrtUpdateTime' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\Single-Sign-on-Info.txt
+dsregcmd /status | select-string -Pattern 'RefreshPrtDiagnostics' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\Single-Sign-on-Info.txt
 
 Write-host 'System Information'
-systeminfo | Select-String -Pattern 'OS Name'
-systeminfo | Select-String -Pattern 'OS Version'
-systeminfo | Select-String -Pattern 'Original Install Date'
-systeminfo | Select-String -Pattern 'System Boot Time'
-systeminfo | Select-String -Pattern 'Time Zone'
-systeminfo | Select-String -Pattern 'Total Physical Memory'
+systeminfo | Select-String -Pattern 'OS Name' | Out-File -FilePath C:\SystemDiagnosticCollection\System-Information.txt
+systeminfo | Select-String -Pattern 'OS Version' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\System-Information.txt
+systeminfo | Select-String -Pattern 'Original Install Date' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\System-Information.txt
+systeminfo | Select-String -Pattern 'System Boot Time' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\System-Information.txt
+systeminfo | Select-String -Pattern 'Time Zone' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\System-Information.txt
+systeminfo | Select-String -Pattern 'Total Physical Memory' | Out-File -Append -FilePath C:\SystemDiagnosticCollection\System-Information.txt
